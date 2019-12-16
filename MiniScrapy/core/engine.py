8381ab1d57b8abf46e163d4d9deba356ed4fb7d1
lay <javs_shao@163.com>
2019-12-16 15:34:55 +0800

    中间件的实现

--- MiniScrapy/core/engine.py
+++ MiniScrapy/core/engine.py
@@ -3,6 +3,8 @@ from MiniScrapy.core.scheduler import Scheduler
 from MiniScrapy.core.pipeline import Pipeline
 from MiniScrapy.core.downloader import Downloader
 from https.request import Request
+from MiniScrapy.middlewares.downloader_middlewares import DownloaderMiddleware
+from MiniScrapy.middlewares.spider_middlewares import SpiderMiddleware
 
 
 class Engine():
@@ -12,16 +14,22 @@ class Engine():
         self.downloader = Downloader()
         self.pipeline = Pipeline()
 
+        self.spider_mids = SpiderMiddleware()
+        self.downloader_mids = DownloaderMiddleware()
+
     def start(self):
         self._start_engine()
 
     def _start_engine(self):
         start_request = self.spider.start_requests()
+        start_request = self.spider_mids.process_response(start_request)
         self.scheduler.add_request(start_request)
         request = self.scheduler.get_request()
         response = self.downloader.get_response(request)
+        response = self.spider_mids.process_response(response)
         result = self.spider.parse(response)
         if isinstance(result, Request):
+            result = self.spider_mids.process_request(result)
             self.scheduler.add_request(result)
         else:
             self.pipeline.process_item(result)
